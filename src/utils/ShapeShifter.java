e8d981b19d4b3879eebff010045d9b845f91635e
Guillaume <onemademu@gmail.com>
2021-09-03 16:31:04 -0400

    ajout d'un commentaire

--- src/utils/ShapeShifter.java
+++ src/utils/ShapeShifter.java
@@ -16,8 +16,8 @@ import net.sf.json.JSONObject;
 public class ShapeShifter {
 
     public static String shapesList() {
-//Je m'apelle pas Guillaumeaaa
-        return null; 
+        //Fait ta job caliss!!!
+        return null;
     }
 
     public static String shapeOne(String nameOfShape, double val1) {
